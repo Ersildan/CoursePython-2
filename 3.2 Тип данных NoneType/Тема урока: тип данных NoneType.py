13c149e1fa1b115d478901269be33ffce9c0fa84
Andrey Svistunov <120823381+Ersildan@users.noreply.github.com>
2023-11-17 21:27:59 +0300

    знакомство с None

---
+++ 3.2 Тип данных NoneType/Тема урока: тип данных NoneType.py
@@ -0,0 +1,17 @@
+'''
+var = None
+if var is None:  # используем оператор is
+  print('None')
+else:
+  print('Not None')
+#None
+'''
+
+'''
+var = None
+if var == None:  # используем оператор ==
+  print('None')
+else:
+  print('Not None')
+#None
+'''
