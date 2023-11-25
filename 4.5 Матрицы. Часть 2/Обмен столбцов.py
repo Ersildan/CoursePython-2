c0468fa52d657821e473c0d2cab449b1d6d53fd9
Andrey Svistunov <120823381+Ersildan@users.noreply.github.com>
2023-11-25 13:26:34 +0300

    +1

--- 4.5 Матрицы. Часть 2/Обмен столбцов.py
+++ 4.5 Матрицы. Часть 2/Обмен столбцов.py
@@ -5,10 +5,10 @@ for i in range(n):
     A.append(temp)
     
 x = [int(i) for i in input().split()]
-a1,a2=x[0], x[1]
+a1, a2 = x[0], x[1]
 
 for i in range(n):
-    A[i][a1], A[i][a2] = A[i][a2],A[i][a1]
+    A[i][a1], A[i][a2] = A[i][a2], A[i][a1]
 for r in range(n):
     for c in range(m):
         print(A[r][c], sep='\n', end=' ')
