dae17460bf85df8a339c84fb70127a1a171a9ffe
Andrey Svistunov <120823381+Ersildan@users.noreply.github.com>
2023-12-27 22:47:09 +0300

    +1
    На вход программе подаются два натуральных числа 
    n и m. Напишите программу, которая создает матрицу размером 
    n×m, заполнив её "змейкой" в соответствии с образцом.

---
+++ 4.6 Матрицы. Часть 3/Заполнение змейкой.py
@@ -0,0 +1,13 @@
+n, m = [int(i) for i in input().split()]
+matrix = [[0] * m for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        matrix[i][j] = i * m + j + 1
+    if i % 2 == 1:
+        matrix[i].reverse()    
+
+for i in range(n):
+    for j in range(m):
+        print(str(matrix[i][j]).ljust(3), end=' ')
+    print()
