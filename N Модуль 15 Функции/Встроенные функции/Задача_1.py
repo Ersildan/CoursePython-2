522cec162905217b2bacbc8a15d40cb718baa27d
Andrey Svistunov <11free11@mail.ru>
2024-04-12 23:43:14 +0300

    Working with functions.

---
+++ N Модуль 15 Функции/Встроенные функции/Задача_1.py
@@ -0,0 +1,5 @@
+def ignore_command(command):
+    ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
+    return any(list(map(lambda x: x in command, ignore)))
+
+
